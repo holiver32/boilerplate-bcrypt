787042f7fc4dfaf9b414e909c66f97bfccc9ca50
HoliveR32 <holiver.hurtado7@gmail.com>
2025-06-29 18:10:48 +0200

    Update server.js
    Add function cors for correct testing

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const cors = require('cors');
+app.use(cors());
 
 fccTesting(app);
 const saltRounds = 12;
